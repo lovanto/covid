1475c97de4bfa44e62dd023cd9ea4cff430a2800
lovanto <rifkylovanto@gmail.com>
2020-06-15 11:12:06 +0700

    convert side-menu to custom-component

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import "./assets/scss/style.scss";
 
 import "./script/component/header-navigation.js"
 import "./script/component/main-menu.js"
+import "./script/component/side-menu.js"
 
 import main from "./script/view/main.js";
 
