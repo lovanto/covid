72b558a74cbcbcd7ab1d8b503810f8748f441119
lovanto <rifkylovanto@gmail.com>
2020-06-23 14:56:22 +0700

    import image

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ import "regenerator-runtime";
 import "./assets/font/Nexa.woff";
 import "./assets/font/SFProDisplay-Regular.woff";
 import "./assets/scss/style.scss";
+import "./assets/img/confirmed.png";
+import "./assets/img/deaths.png";
+import "./assets/img/recovered.png";
+import "./assets/img/virusTotal.png";
 
 import "./script/component/header-navigation.js"
 import "./script/component/main-menu.js"
